d9a56d11bff34893507b93eaa2836417e33195d6
Jeongmo <wjdah1402@gmail.com>
2023-01-01 12:38:05 +0900

    Switch places of function

--- Test/Calculator.c
+++ Test/Calculator.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
-int main(void)
-{
-  double number1 = 3, number2 = 4, result;
-  char Arithmetic = '*';
-
-  result = Calculate(number1, Arithmetic, number2);
-  print("Answer is %d", result)
-  return 0;
-}
-
-double Calculate(double number1, char Arithmetic, double number2)
+double calculate(double number1, char Arithmetic, double number2)
 {
   double sum = 0;
   switch(Arithmetic)
@@ -28,4 +18,15 @@ double Calculate(double number1, char Arithmetic, double number2)
       sum = number1 - number2;
       break;
   }
+  return 0;
+}
+
+int main(void)
+{
+  double number1 = 3, number2 = 4, result;
+  char Arithmetic = '*';
+
+  result = number1 * number2; //calculate(number1, Arithmetic, number2);
+  printf("Answer is %f", result);
+  return 0;
 }

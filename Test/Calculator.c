ea933509ec587262a431490130747ad0364a61f2
Jeongmo <wjdah1402@gmail.com>
2023-01-01 12:05:30 +0900

    Add function Calculate

--- Test/Calculator.c
+++ Test/Calculator.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-  
+  double number1 = 3, number2 = 4, result;
+  char Arithmetic = '*';
+
+  result = Calculate(number1, Arithmetic, number2);
+  print("Answer is %d", result)
   return 0;
+}
+
+double Calculate(double number1, char Arithmetic, double number2)
+{
+  double sum = 0;
+  switch(Arithmetic)
+  {
+    case '*' :
+      sum = number1 * number2;
+      break;
+    case '/' :
+      sum = number1 / number2;
+      break;
+    case '+' :
+      sum = number1 + number2;
+      break;
+    case '-' :
+      sum = number1 - number2;
+      break;
+  }
 }
